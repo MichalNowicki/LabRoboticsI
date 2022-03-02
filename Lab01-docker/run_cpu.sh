4a86fe09e684db0bfb07cfd76c011d90b2c7dd55
michalnowicki <michal.nowicki@put.poznan.pl>
2022-03-02 10:41:53 +0100

    Correcting scripts

--- Lab01-docker/run_cpu.sh
+++ Lab01-docker/run_cpu.sh
@@ -13,7 +13,7 @@ XAUTH=/tmp/.docker.xauth
      chmod a+r $XAUTH
  fi
 
-docker stop SIwR_01 || true && docker rm SIwR_01 || true
+docker stop ROB_01 || true && docker rm ROB_01 || true
 
 docker run -it \
     --env="DISPLAY=$DISPLAY" \
