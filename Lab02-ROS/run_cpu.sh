3cbbf0b15d44eadf37e4f9775c3b0223426670c4
Michal Nowicki <Michal Nowicki>
2022-03-08 22:06:14 +0100

    Lab02 docker commands

---
+++ Lab02-ROS/run_cpu.sh
@@ -0,0 +1,30 @@
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker stop LabRobotics02 || true && docker rm LabRobotics02 || true
+
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --privileged \
+    --network=host \
+    --name="LabRobotics02" \
+    osrf/ros:noetic-desktop-full \
+    /bin/bash
