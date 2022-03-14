484b02a903e893e1c08122064e5115725936bb4f
Michal Nowicki <Michal Nowicki>
2022-03-14 10:25:56 +0100

    Adding codes for Lab03

---
+++ Lab03-ROS_part2/rob_03_run_gpu_nvidia.sh
@@ -0,0 +1,31 @@
+xhost +local:root
+
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker stop LabRobotics03 || true && docker rm LabRobotics03 || true
+
+docker run -it \
+    --gpus all \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --env="NVIDIA_VISIBLE_DEVICES=all" \
+    --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+    --privileged \
+    --network=host \
+    --name="LabRobotics03" \
+    rob/image03 \
+    /bin/bash
